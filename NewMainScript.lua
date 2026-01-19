1e5aad21730fffd97f70f8c508246f285d5809ad
jazzy <brittaneymerritt71@gmail.com>
2026-01-18 20:53:58 -0800

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -348,8 +348,55 @@ if game.PlaceId == 6872265039 then
 				return rawget(self, ind)
 			end
 		})
-		print(lplr.Name)
 	end)
+	run(function()
+		local Sprint
+		local old
+		
+		Sprint = vapelite:CreateModule({
+			Name = 'Sprint',
+			Function = function(callback)
+				if callback then
+					if inputService.TouchEnabled then pcall(function() lplr.PlayerGui.MobileUI['2'].Visible = false end) end
+					old = bedwars.SprintController.stopSprinting
+					bedwars.SprintController.stopSprinting = function(...)
+						local call = old(...)
+						bedwars.SprintController:startSprinting()
+						return call
+					end
+					Sprint:Clean(entitylib.Events.LocalAdded:Connect(function() bedwars.SprintController:stopSprinting() end))
+					bedwars.SprintController:stopSprinting()
+				else
+					if inputService.TouchEnabled then pcall(function() lplr.PlayerGui.MobileUI['2'].Visible = true end) end
+					bedwars.SprintController.stopSprinting = old
+					bedwars.SprintController:stopSprinting()
+				end
+			end,
+			Tooltip = 'Sets your sprinting to true.'
+		})
+	end)	
+	run(function()
+		local MHA
+		MHA = vapelite:CreateModule({
+			Name = "ViewHistory",
+			Function = function(callback)
+				if callback then
+					bedwars.MatchHistroyController:requestMatchHistory(lplr.Name):andThen(function(Data)
+						if Data then
+							bedwars.AppController:openApp({
+								app = bedwars.MatchHistroyApp,
+								appId = "MatchHistoryApp",
+							}, Data)
+						end
+					end)
+					MHA:Toggle(false)
+				else
+					return
+				end
+			end,
+			Tooltip = "allows you to see peoples history without being in the same game with you"
+		})																								
+	end)	
 end
 
 table.insert(vapelite.Connections, web.OnMessage:Connect(vapelite.Receive))
